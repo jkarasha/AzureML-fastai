3c32337a25b2d300f4b74504d29f51730a3d30d7
Joe Karasha <jkarasha@gmail.com>
2023-02-15 20:20:09 -0800

    Use azure user to install custom script.

--- fastaionAMLCI.sh
+++ fastaionAMLCI.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-conda create -y --name fastaienv
-source /etc/profile.d/conda.sh
-conda activate fastaienv
+
+set -e
+
+# This script installs fastai, ipykernel packages in compute instance azureml_py38 environment.
+
+sudo -u azureuser -i <<'EOF'
+
+ENVIRONMENT=fastaienv 
+conda create -y --name "$ENVIRONMENT"
+conda activate "$ENVIRONMENT"
 conda install -y pip
 conda install -y ipykernel
 conda install -c fastchan fastai anaconda
 python -m ipykernel install --user --name fastaienv --display-name "Python (fastaienv)"
+conda deactivate
+EOF
