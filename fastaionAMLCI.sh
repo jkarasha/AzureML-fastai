10db4166f45a68f132b19ea3953c9881f89fc5e5
Joe Karasha <jkarasha@gmail.com>
2023-02-15 20:36:06 -0800

    Parameterize kernel name.

--- fastaionAMLCI.sh
+++ fastaionAMLCI.sh
@@ -6,12 +6,13 @@ set -e
 
 sudo -u azureuser -i <<'EOF'
 
+PYKERNAL="Python (fastaienv)"
 ENVIRONMENT=fastaienv 
 conda create -y --name "$ENVIRONMENT"
 conda activate "$ENVIRONMENT"
 conda install -y pip
 conda install -y ipykernel
 conda install -c fastchan fastai anaconda
-python -m ipykernel install --user --name fastaienv --display-name "Python (fastaienv)"
+python -m ipykernel install --user --name fastaienv --display-name "$PYKERNAL"
 conda deactivate
 EOF
