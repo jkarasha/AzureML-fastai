c1f0729f6fe3c1582fc333a9a5342c557fa78784
Joe Karasha <jkarasha@gmail.com>
2023-02-15 18:55:01 -0800

    Update for course-22

--- fastaionAMLCI.sh
+++ fastaionAMLCI.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-git clone https://github.com/fastai/fastai.git
-git clone https://github.com/fastai/fastbook.git
 conda create -y --name fastaienv
 source /etc/profile.d/conda.sh
 conda activate fastaienv
 conda install -y pip
 conda install -y ipykernel
-conda install -y -c fastai -c pytorch fastai
-conda install -y -c fastai fastbook
-conda install -y -c fastai nbdev
+conda install -c fastchan fastai anaconda
 python -m ipykernel install --user --name fastaienv --display-name "Python (fastaienv)"
